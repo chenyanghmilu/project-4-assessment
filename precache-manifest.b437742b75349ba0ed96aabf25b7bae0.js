778968321e5254d40405fa0fddc2b99ec828cd76
Yang Chen <chenyanghmilu@gmail.com>
2020-02-14 17:17:58 -0500

    Updates

---
+++ precache-manifest.b437742b75349ba0ed96aabf25b7bae0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e91af887c9188c42b20703dfebbeda",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "4c336f9f71c2a9726995",
+    "url": "/project-4-assessment/static/css/main.9b9fb508.chunk.css"
+  },
+  {
+    "revision": "50791072ca96be2954d2",
+    "url": "/project-4-assessment/static/js/2.4a494911.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/project-4-assessment/static/js/2.4a494911.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c336f9f71c2a9726995",
+    "url": "/project-4-assessment/static/js/main.dc43eaad.chunk.js"
+  },
+  {
+    "revision": "9923933f7998bf1ba10e",
+    "url": "/project-4-assessment/static/js/runtime-main.6f46026f.js"
+  }
+]);
